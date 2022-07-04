58df74a8c97f19e78736d294e1b099a017984c7c
euheber <euheber1@gmail.com>
2022-07-03 21:56:54 -0300

    fix: toggle rotate on button

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,7 @@
-const btn = document.querySelector('span')
+const btn = document.querySelector('button')
 const mobileMenu = document.querySelector('#mobile-menu')
 
 btn.addEventListener('click',()=> { 
+    btn.classList.toggle('rotate')
     mobileMenu.classList.toggle('on')
 })
